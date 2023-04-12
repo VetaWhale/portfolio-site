739256ab7288835021e34bbc9ce02673046b5933
VetaWhale <veta-whale@yandex.ru>
2023-04-12 11:58:36 +0300

    added burger

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const elements = document.querySelectorAll('.my-skills__card_title-content');
 
-for(let element of elements) {
-    element.onclick = function() {
-        if (element.classList.contains('close')) {
-            element.classList.add('open');
-            element.classList.remove('close')
-        } else {
-            element.classList.add('close')
-            element.classList.remove('open');
-        }
+const BURGER_OPENED_CLASSNAME = 'burger_open';
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BODY_FIXED_CLASSNAME = 'body_fixed';
+
+const bodyNode = document.querySelector('body');
+const burgerNode = document.querySelector('.js-burger');
+const burgerBtnNode = document.querySelector('.js-burger-btn');
+const burgerContentNode = document.querySelector('.js-burger__content')
+
+burgerBtnNode.addEventListener('click', toggleBurger);
+
+burgerNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
     }
+})
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
 }
+
 const cells = document.querySelectorAll('.portfolio-card');
 const button = document.querySelector('.portfolio-card__button')
 
